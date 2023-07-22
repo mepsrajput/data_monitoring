188a67d36ab57cbc4dd7c3f28a4d22eb032ad776
Pradeep Singh <viveksingh12126@gmail.com>
2023-07-23 01:12:44 +0530

    Update and rename data_profiling.py to data_profiler.py

---
+++ data_profiler.py
@@ -0,0 +1,44 @@
+# data_profiler.py
+import argparse
+import pyspark.sql.functions as F
+from pyspark.sql import SparkSession
+from tabulate import tabulate
+from utils import determine_variable_type, calculate_count_missing, calculate_numeric_summary, calculate_categorical_summary, format_summary
+import logging
+
+# Logging Configuration
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+
+# Magic Numbers
+APPROX_QUANTILE_ERROR = 0.01
+OUTLIER_THRESHOLD = 1.5
+
+def profile_df(df, summary_level):
+    # The same profile_df function from the previous code goes here
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Profile a Spark DataFrame.")
+    parser.add_argument("--data_path", type=str, required=True, help="Path to the data file.")
+    parser.add_argument("--summary_level", type=str, choices=["summary", "detailed"], default="summary", help="Level of detail in the summary.")
+    parser.add_argument("--log_level", type=str, choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], default="INFO", help="Logging level (optional).")
+    args = parser.parse_args()
+
+    # Set the logging level
+    logging.basicConfig(level=args.log_level, format='%(levelname)s: %(message)s')
+
+    # Input Validation
+    if not args.data_path:
+        logging.error("Data path not provided. Please provide the path to the data file using --data_path argument.")
+        exit(1)
+
+    # Initialize Spark session.
+    spark = SparkSession.builder.appName("Data Profile").getOrCreate()
+
+    # Load the data.
+    df = spark.read.csv(args.data_path, inferSchema=True, header=True)
+
+    # Profile the data.
+    profile_df(df, summary_level=args.summary_level)
+
+    # Stop Spark session after usage
+    spark.stop()
